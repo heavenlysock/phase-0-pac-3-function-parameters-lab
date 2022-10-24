fb4b8c293abcb184aee1913d73a5873f9a8c02da
bobertsworld <bobertsworld@gmail.com>
2022-10-23 20:02:28 -0700

    commit tree 5543ac695e837854e0d4df60ff4b8f616c0e4f22

---
+++ index.js
@@ -0,0 +1,9 @@
+// function introduction(name) {
+//     console.log('Hi, my name is ${name}');
+
+// }
+
+
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
